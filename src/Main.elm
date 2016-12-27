e3ccb75c0c927e8b1fe6cb4fda2ac88a240f8f4a
Christophe Benz <christophe.benz@gmail.com>
2016-12-27 16:00:08 +0100

    Add navbar and disclaimer

--- src/Main.elm
+++ src/Main.elm
@@ -1,8 +1,8 @@
 module Main exposing (..)
 
 import Html exposing (..)
---import Html.Attributes exposing (..)
---import Html.Events exposing (onClick)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onClick)
 
 
 main : Program Never Model Msg
@@ -20,16 +20,17 @@ main =
 
 
 type alias Model =
-    {}
+    { displayDisclaimer : Bool }
 
 
 initialModel : Model
 initialModel =
-    {}
+    { displayDisclaimer = True }
 
 
 init : ( Model, Cmd Msg )
 init =
+    -- TODO Load displayDisclaimer from flags and store setting in localStorage.
     ( initialModel, Cmd.none )
 
 
@@ -38,13 +39,13 @@ init =
 
 
 type Msg
-    = NoOp
+    = CloseDisclaimer
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        NoOp ->
+        CloseDisclaimer ->
             ( model, Cmd.none )
 
 
@@ -64,6 +65,114 @@ subscriptions model =
 view : Model -> Html Msg
 view model =
     div []
-        [ text "Hello, world!"
-        , text (toString model)
+        ([ viewNavBar
+         , div [ class "container-fluid" ]
+            ((if model.displayDisclaimer then
+                [ viewDisclaimer ]
+              else
+                []
+             )
+                ++ []
+            )
+         ]
+        )
+
+
+viewDisclaimer : Html Msg
+viewDisclaimer =
+    div [ class "alert alert-warning disclaimer" ]
+        [ button
+            [ attribute "aria-hidden" "true"
+            , attribute "data-dismiss" "alert"
+            , class "close"
+            , onClick CloseDisclaimer
+            , type_ "button"
+            ]
+            [ text "×" ]
+        , h4 [] [ text "À propos de cet outil" ]
+        , ul []
+            [ li [] [ text "OpenFisca est un simulateur socio-fiscal en cours de développement." ]
+            , li [] [ text "Les résultats des simulations peuvent comporter des erreurs, et vous pouvez commencer à contribuer au projet en les faisant remonter si vous les détectez." ]
+            , li [] [ text "Cet outil est un démonstrateur pour OpenFisca qui illustre ses possibilités concernant des cas types." ]
+            , li [] [ text "Les données que vous saisissez ne sont jamais stockées." ]
+            ]
+        , p []
+            [ strong [] [ text "Les résultats affichés n'ont en aucun cas un caractère officiel." ] ]
+          -- , button -- TODO Store setting in localStorage
+          --     [ class "btn btn-link"
+          --     , onClick CloseDisclaimer
+          --     ]
+          --     [ text "J'ai compris, ne plus afficher" ]
+        ]
+
+
+viewNavBar : Html msg
+viewNavBar =
+    nav [ class "navbar navbar-inverse navbar-static-top", attribute "role" "navigation" ]
+        [ div [ class "container" ]
+            [ div [ class "navbar-header" ]
+                [ button
+                    [ class "navbar-toggle"
+                    , attribute "data-target" "#topbar-collapse"
+                    , attribute "data-toggle" "collapse"
+                    , type_ "button"
+                    ]
+                    [ span [ class "sr-only" ]
+                        [ text "Basculer la navigation" ]
+                    , span [ class "icon-bar" ]
+                        []
+                    , span [ class "icon-bar" ]
+                        []
+                    , span [ class "icon-bar" ]
+                        []
+                    ]
+                , a [ class "navbar-brand", href "https://ui.openfisca.fr/" ]
+                    -- TODO Add to program flags
+                    [ text "Démonstrateur OpenFisca" ]
+                ]
+            , div [ class "collapse navbar-collapse", id "topbar-collapse" ]
+                [ ul [ class "nav navbar-nav" ]
+                    []
+                , ul [ class "nav navbar-nav navbar-right" ]
+                    [ li []
+                        [ a [ href "https://www.openfisca.fr/" ]
+                            -- TODO Add to program flags
+                            [ text "Retour à l'accueil" ]
+                        ]
+                    , li [ class "visible-xs-block" ]
+                        [ a [ href "http://stats.data.gouv.fr/index.php?idSite=4" ]
+                            -- TODO Add to program flags
+                            [ text "Statistiques du site" ]
+                        ]
+                    , li [ class "visible-xs-block" ]
+                        [ a [ href "https://www.openfisca.fr/mentions-legales" ]
+                            -- TODO Add to program flags
+                            [ text "Mentions légales" ]
+                        ]
+                      -- , li [ class "visible-xs-block" ] -- TODO
+                      --     [ a [ href "/privacy-policy" ]
+                      --         [ text "Politique de confidentialité" ]
+                      --     ]
+                      -- , li [ class "dropdown" ] -- TODO
+                      --     [ a [ class "dropdown-toggle", attribute "data-toggle" "dropdown", href "#" ]
+                      --         [ text "Français "
+                      --         , span [ class "caret" ]
+                      --             []
+                      --         ]
+                      --     , ul [ class "dropdown-menu", attribute "role" "menu" ]
+                      --         [ li [ class "dropdown-header", attribute "role" "presentation" ]
+                      --             [ text "France" ]
+                      --         , li []
+                      --             [ a [ href "/" ]
+                      --                 [ text "Français" ]
+                      --             ]
+                      --         , li []
+                      --             [ a [ href "/en" ]
+                      --                 [ text "English" ]
+                      --             ]
+                      --         ]
+                      --     ]
+                    ]
+                ]
+            ]
         ]
